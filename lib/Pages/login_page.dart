4bef88f2519829330ba3f1fcdfe354600c81ae9b
Rahul <joinmerahul5714@gmail.com>
2022-07-27 16:05:15 +0530

    Day6 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:myfirst_one/utills/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changebutton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +25,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
@@ -37,6 +44,11 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Username",
                         labelText: "Username",
                       ),
+                      onChanged: (value) {
+                        name = value;
+                        // to call build function again.
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -48,16 +60,48 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 20.0,
                     ),
-                    ElevatedButton(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        }),
+                    //button designed by myself.
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changebutton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changebutton ? 80 : 120,
+                          height: 40,
+                          alignment: Alignment.center,
+                          child: changebutton
+                              ? Icon(Icons.done, color: Colors.white)
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            // shape: changebutton
+                            //     ? BoxShape.circle
+                            //     : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changebutton ? 50 : 8),
+                          )),
+                    ),
+                    // ElevatedButton(
+                    //     child: Text(
+                    //       "Login",
+                    //       style: TextStyle(
+                    //           fontSize: 20, fontWeight: FontWeight.bold),
+                    //     ),
+                    //     style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+                    //     onPressed: () {
+                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //     }),
                   ],
                 ),
               )
